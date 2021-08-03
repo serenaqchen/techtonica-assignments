146136ad9def70561791a34e21e7fad2e32aa19d
Serena <serenaqchen@gmail.com>
2021-08-02 22:53:44 -0700

    added new functions to event object, but addAvailableTicket function is not working

--- OOP Mini Project/event.js
+++ OOP Mini Project/event.js
@@ -4,8 +4,42 @@ class Event {
     this.name = name;
     this.description = description;
     this.availableTickets = [];
+    this.addAvailableTickets = function(name, price){
+        this.availableTickets.push(new TicketType(name, price));
+    };
+    this.allTickets = function () {
+        let result = "";
+        for (let i = 0; i <= this.addAvailableTickets.length; i++){
+            result = result.concat(` ${i+1}. ${this.availableTickets[i].name} ($${this.availableTickets[i].price})`)
+            console.log(i)
+      }
+        return 'All tickets:'.concat(result)
+    };
+    this.searchTickets = function (lower, upper) {
+        let result = "";
+        for (let i = 0; i < this.addAvailableTickets.length; i++){
+            let ticketPrice = this.availableTickets[i].price
+            console.log(ticketPrice)
+            if (ticketPrice > lower && ticketPrice < upper){
+                result = result.concat(` ${i+1}. ${this.availableTickets[i].name} ($${this.availableTickets[i].price})`);
+            }
+        }
+        if (result.length === 0){
+            return "No tickets available.";
+        } else {
+            return 'Eligible:'.concat(result);
+        }
+    };
   }
 }
+//create new class 
+class TicketType {
+    constructor (name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
 
 //below statement creates an object
 const eventObj1 = new Event(
@@ -26,13 +60,27 @@ eventArray.push(eventObj1, eventObj2, eventObj3);
 // in order to check whether the elements are pushed, use console.log
 console.log(eventArray);
 
+//adding new ticket types
+eventObj1.addAvailableTickets("human", 299);
+eventObj1.addAvailableTickets("vampire", 99);
+eventObj2.addAvailableTickets("General Admission", 25);
+eventObj2.addAvailableTickets("Floor Seating", 80);
+eventObj3.addAvailableTickets("Balcony", 100);
+eventObj3.addAvailableTickets("Orchestra", 300);
+eventObj3.addAvailableTickets("Mezzanine", 200);
+eventObj3.addAvailableTickets("serena", 90);
+
+
+console.log(eventObj3.availableTickets)
+console.log(eventObj3.searchTickets(10, 110))
+
 //select html element 
 let eventList = document.getElementById('event');
 
 //iterate through the list of objects and add a new element, li, to the parent with the event name and description
 for (let anEvent of eventArray){
     let li = document.createElement('li');
-    li.innerHTML = `${anEvent['name']} - ${anEvent['description']}`;
+    li.innerHTML = `${anEvent['name']} - ${anEvent['description']} - ${anEvent.searchTickets(10, 110)}`;
     eventList.appendChild(li);
 }
 
