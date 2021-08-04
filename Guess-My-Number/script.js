44004be97abd8b0cb6643056e0ae8b030a99b29f
Serena <serenaqchen@gmail.com>
2021-08-03 23:05:07 -0700

    created basic guess my number game (unfinished)
    made function to check if number is correct, if not, then add one to wrongGuesses and guessed number to prevGuess array. Still need to write conditions for when guessed wrong.

---
+++ Guess-My-Number/script.js
@@ -0,0 +1,36 @@
+
+//get a random secret number between 0 and 100
+let secretNum = Math.floor(Math.random() * 100);
+
+//count number of wrong guesses
+let wrongGuesses = 0;
+
+//list of previous guesses
+let prevGuesses = [];
+
+//get input from user 
+//going to have to use DOM methods to get input 
+function checkNum(){
+    //getting users guess
+    let userGuess = document.getElementById("guessNum").value;
+    //checking if guess is correct
+    if (userGuess == secretNum){
+        //add a notification that they got it correct 
+        let notification = document.createElement('p')
+        if (wrongGuesses === 0) {
+            notification.innerHTML = `Congratulations! You guessed the secret number in ${wrongGuesses + 1} try!`
+        } else {
+            notification.innerHTML = `Congratulations! You guessed the secret number in ${wrongGuesses + 1} tries!`
+        }
+        document.getElementById('notification').appendChild(notification)
+    } else {
+        wrongGuesses += 1;
+        prevGuesses.push(userGuess);
+        let notification = document.createElement('p')
+        notification.innerHTML = `Guess again!`
+        document.getElementById('notification').appendChild(notification)
+    }
+}
+
+//add event listener for when user submits a guess
+document.querySelector('button').addEventListener('click', checkNum);
