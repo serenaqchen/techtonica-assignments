d404f80048c7eccc58be0480e960315303a1de7d
Serena <serenaqchen@gmail.com>
2021-08-11 11:01:18 -0700

    Initial commit of tic tac toe game

---
+++ tic-tac-toe/src/components/Game.js
@@ -0,0 +1,23 @@
+import { func } from 'prop-types';
+import React from 'react';
+import Board from './Board';
+
+function Game(){
+  function handleClick(){
+
+  }
+
+  function jumpTo(){
+
+  }
+
+  function renderMoves(){
+
+  }
+  
+  return (
+    <Board onClick={handleClick} />
+  )
+}
+
+export default Game;
