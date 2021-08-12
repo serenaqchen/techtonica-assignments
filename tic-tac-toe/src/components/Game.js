e8a5e6d820b6f5b7bc0d17f02a66f66188b030b8
Serena <serenaqchen@gmail.com>
2021-08-12 15:21:22 -0700

    Added comments to code

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -3,13 +3,20 @@ import calculateWinner from '../helpers';
 import Board from './Board';
 import './Game.css';
 
+//creating the game component that has hooks
 function Game(){
+  //creates a hook for managing the board when it is clicked
+  //initializing it with an empty(null) 3x3 board
+  //setBoard is a function that updates board
   const [board, setBoard] = useState(Array(9).fill(null));
+  //create a hook for managing the next player. xIsNext is set to true this means that X is going 
   const [xIsNext, setXisNext] = useState(true);
+  //winner returns the winner of the game by indexing sqaures or returns null if there are no winners
   const winner = calculateWinner(board);
   
-
+  //this function runs when a square is clicked on the board 
   function handleClick(i){
+    //making a copy of the current status of the board
     const boardCopy = [...board];
     // If user click an occupied square or if game is won, return
     if (winner || boardCopy[i]) return;
--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Square.css';
 
+//this component returns each individual square for the tic tac toe board
+//takes in a value(what is shown in the box) and onClick(fxn, what happens when you click the square)
 function Square({ value, onClick}){
   return (
+    //want to return a button what runs the onClick function when clicked and displays the value that is passed from the prop
     <button className="square" onClick={onClick}>{value}</button>
   )
 }
