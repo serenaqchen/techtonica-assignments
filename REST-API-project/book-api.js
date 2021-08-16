aa39741b9c3beacb2c22793a48d5571b4ffd5633
Serena <serenaqchen@gmail.com>
2021-08-16 15:07:56 -0700

    initial commit of REST API project

---
+++ REST-API-project/book-api.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Where we will keep books
+let books = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    // We will be coding here
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
