153acb5ceec97d9c9a297f45455f6c1094fdd983
Serena <serenaqchen@gmail.com>
2021-08-18 10:08:54 -0700

    Created route for posting books

--- REST-API-project/book-api.js
+++ REST-API-project/book-api.js
@@ -1,9 +1,12 @@
+//importing the express framework
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+//creating the express app
 const app = express();
-const port = 3000;
+//setting the port
+const port = process.env.PORT || 3000;
 
 // Where we will keep books
 let books = [];
@@ -14,8 +17,19 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World, from Express');
+  });
+
 app.post('/book', (req, res) => {
-    // We will be coding here
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
+
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
