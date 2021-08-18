ab1eaf655d0fb7deeb8908c2c31f50f15c7201cf
Serena <serenaqchen@gmail.com>
2021-08-18 12:54:10 -0700

    Create & read actions are working but still working update & delete

--- REST-API-project/book-api.js
+++ REST-API-project/book-api.js
@@ -17,10 +17,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//what you get on the main page localhost:3000
 app.get('/', (req, res) => {
     res.send('Hello World, from Express');
   });
 
+  //adding books to the book list
 app.post('/book', (req, res) => {
     const book = req.body;
 
@@ -31,5 +33,60 @@ app.post('/book', (req, res) => {
     res.send('Book is added to the database');
 
 });
+
+//getting the book list
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+//Retrieving a Book by ISBN
+app.get('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // Searching books for the isbn
+    for (let book of books) {
+        if (book.isbn === isbn) {
+            res.json(book);
+            return;
+        }
+    }
+
+    // Sending 404 when not found something is a good practice
+    res.status(404).send('Book not found');
+});
+
+//Deleting Books
+app.delete('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // Remove item from the books array
+    books = books.filter(i => {
+        if (i.isbn !== isbn) {
+            return true;
+        }
+        return false;
+    });
+
+    res.send('Book is deleted');
+});
+
+//editing Books
+// app.post('/book/:isbn', (req, res) => {
+//     // Reading isbn from the URL
+//     const isbn = req.params.isbn;
+//     const newBook = req.body;
+
+//     // Remove item from the books array
+//     for (let i = 0; i < books.length; i++) {
+//         let book = books[i]
+//         if (book.isbn === isbn) {
+//             books[i] = newBook;
+//         }
+//     }
+
+//     res.send('Book is edited');
+// });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
